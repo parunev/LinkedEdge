b58ab4cc3cc7de0da7d6c9fe38b383baff550516
Martin Parunev <martin.parunev@university.mentormate.com>
2023-09-26 14:49:08 +0300

    init

--- src/main/java/com/parunev/linkededge/util/RequestUtil.java
+++ src/main/java/com/parunev/linkededge/util/RequestUtil.java
@@ -1,8 +1,11 @@
 package com.parunev.linkededge.util;
 
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class RequestUtil {
     public static String getCurrentRequest() {
         ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.currentRequestAttributes();
