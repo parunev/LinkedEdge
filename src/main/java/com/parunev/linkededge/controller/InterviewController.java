5416193d585c62d9b996ad8586b7bc4eeb0e3ba0
Martin Parunev <martin.parunev@university.mentormate.com>
2023-10-13 19:02:55 +0300

    interview controller documentation

--- src/main/java/com/parunev/linkededge/controller/InterviewController.java
+++ src/main/java/com/parunev/linkededge/controller/InterviewController.java
@@ -2,6 +2,10 @@ package com.parunev.linkededge.controller;
 
 import com.parunev.linkededge.model.payload.interview.*;
 import com.parunev.linkededge.service.InterviewService;
+import com.parunev.linkededge.util.annotations.openapi.interview.ApiAnswerSpecificQuestion;
+import com.parunev.linkededge.util.annotations.openapi.interview.ApiGenerateRandomQuestions;
+import com.parunev.linkededge.util.annotations.openapi.interview.ApiPrepareMeForJob;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,22 +20,26 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/edge-api/v1/interview")
+@Tag(name = "Interview Controller", description = "Endpoints related to interviews and job preparations")
 public class InterviewController {
 
     private final InterviewService interviewService;
 
+    @ApiGenerateRandomQuestions
     @GetMapping("/generate")
     @PreAuthorize("hasAnyRole('ROLE_USER', 'ROLE_USER_EXTRA')")
     public ResponseEntity<List<QuestionResponse>> generateRandomInterviewQuestions(@RequestBody QuestionRequest request){
        return new ResponseEntity<>(interviewService.generateRandomInterviewQuestions(request), HttpStatus.OK);
     }
 
+    @ApiAnswerSpecificQuestion
     @GetMapping("/answer-me")
     @PreAuthorize("hasAnyRole('ROLE_USER', 'ROLE_USER_EXTRA')")
     public ResponseEntity<AnswerResponse> answerUserQuestion(@RequestBody AnswerRequest request){
         return new ResponseEntity<>(interviewService.answerUserQuestion(request), HttpStatus.OK);
     }
 
+    @ApiPrepareMeForJob
     @GetMapping("/prepare-me")
     @PreAuthorize("hasAnyRole('ROLE_USER', 'ROLE_USER_EXTRA')")
     public ResponseEntity<JobResponse> prepareMeForAJob(@RequestBody JobRequest request){
